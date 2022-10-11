aac7a9d10bcf12730325ead962dd71b31fafcd50
sirekanian <sirekanian@pm.me>
2022-10-11 15:45:05 +0400

    improved verify script

--- verify.sh
+++ verify.sh
@@ -9,13 +9,15 @@ find json -type f -print0 | while IFS= read -r -d '' f; do
     echo "Unexpected type ($type): $f"
     exit 1
   fi
-  length="$(jq -r length "$f")"
-  if [ "$length" -lt 29 ] || [ "$length" -gt 32 ]; then
-    echo "Unexpected length ($length): $f"
-    exit 1
-  fi
+  for URL in $(jq -r '.[] | select(.media_type=="image") | .url' "$f"); do
+    path="${URL#https://apod.nasa.gov/apod/}"
+    if [ ! -f "$path" ] && [ "$URL" != "$path" ]; then
+      echo "Missing $URL"
+      exit 1
+    fi
+  done
 done
 
-find apod.nasa.gov -type f -print0 | while IFS= read -r -d '' f; do
+find image -type f -print0 | while IFS= read -r -d '' f; do
   identify "$f" >/dev/null
 done
